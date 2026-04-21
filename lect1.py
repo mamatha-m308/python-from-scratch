9f0ad942286ffd8731db0a400709250eb2d99a6e
mamatha <167190861+mamatha-m308@users.noreply.github.com>
2026-04-21 22:31:21 +0530

    python basics

---
+++ lect1.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("Good morning everyone, My name is Mamatha M")
---
+++ lect2.py
@@ -0,0 +1,63 @@
+#VARIABLE(must be start with letter or underscore)
+a=10    # assigning the value 10 to a
+b=20
+print(a,b)
+
+
+#DATATYPE
+Name="Mamatha M"   #string
+Age=20             #int
+weight=48.6        #float
+is_student=True    #bool
+
+
+#TYPE CONVERSION
+Salary=20000
+Sal_float=float(Salary)
+print(Sal_float)
+
+num1=10
+num2="20"
+print(num1+int(num2))  #python concatinnate int and int not int and string
+
+
+#ARITHMATIC OPERATION
+a=10
+b=3
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a//b) #flor divission(it will give you the whole nbr)
+print(a%b) #modulus
+print(a**b) #exponentiation(it will add power to 1st nbr based on 2nd nbr)
+
+#swap 2 numbers
+a=50
+b=100
+temp=a
+a=b
+b=temp
+print(a,b)
+
+
+#USER INPUT
+name=input("Enter name")
+age=int(input("enter age"))
+print(f"Name is {name} and Age is {age}")  #  f->formated concatination
+
+#ABSOLUTE VALUE(even if the ans is -ve it will return +ve)
+a=10
+b=20
+c=abs(a-b)
+print(c)
+
+#COMMENTS
+#IN PYTHON USE # FOR SINGLE COMMENT
+#USE """   """  THIS FOR MULTI LINE COMMAND
+
+
+
+
+
+
